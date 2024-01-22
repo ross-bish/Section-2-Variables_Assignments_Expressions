678b26926c557d6063807a3623e748b4f2544c2d
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:34:00 +0000

    Add files via upload

---
+++ 2_20 ASCII code/main.py
@@ -0,0 +1,26 @@
+# ----------------
+# ASCII code
+# Date:
+# ----------------
+
+# Add comments where necessary to describe what your code is doing.
+'''
+character = "A"
+ascii = ord(character)   # converting "A" into a decimal value, storing value in the ascii variable
+print(type(ascii))    # shows data type
+print("The decimal value for the letter A is: ", ascii)
+
+output = chr(ascii)  # converting decimal value to a letter
+print(type(output))  # display data type
+print("The character represented by decimal value", ascii, " is: ", output)
+'''
+
+### Practice
+
+value = input("Enter keyboard value: ")
+ascii = ord(value)
+print("The decimal value of", value, "is", ascii)
+
+decimal = int(input("Enter Unicode value: "))
+unicode = chr(decimal)
+print("The character representation of", decimal, "is", unicode)
