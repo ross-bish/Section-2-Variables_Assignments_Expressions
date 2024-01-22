2c15af74a430fe6dc5d341a262a379eb42f9d78c
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:29:01 +0000

    Add files via upload

---
+++ 2_17 Running Totals/main.py
@@ -0,0 +1,21 @@
+# ---------------------
+# Title: Running Total
+# Date:
+# ---------------------
+
+
+# Program to calculate running total
+
+
+
+
+# Initialise the variable
+
+
+
+
+# Perform the calculations
+
+
+
+# Display the output
