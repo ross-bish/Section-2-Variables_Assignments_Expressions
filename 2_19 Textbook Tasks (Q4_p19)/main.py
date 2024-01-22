3dce13745a992d4f986054770274c55d62c5e3d6
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:32:33 +0000

    Add files via upload

---
+++ 2_19 Textbook Tasks (Q4_p19)/main.py
@@ -0,0 +1,16 @@
+# -----------------------
+# Title:
+# Date:
+# -----------------------
+
+
+
+# Promt user to enter Temperature in degrees Fahrenheit
+
+
+
+# Convert from ℃ to ℉
+
+
+
+# Output message 
