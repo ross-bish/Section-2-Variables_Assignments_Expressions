7b347cde38503e6364498fbe0abb0d9bdb71a0da
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 14:55:08 +0000

    Add files via upload

---
+++ 2.3 Investigate/main.py
@@ -0,0 +1,93 @@
+# ---------------------
+# Variables - Investigate
+# Date:
+# ---------------------
+
+
+### Predict and Run Example 1
+# Add comments to the code below to explain what it does
+
+firstName = "Buzz"  
+
+print(firstName)    
+
+### Predict and Run Example 2
+ # Add comments to the code below to explain what it does
+
+lastName = "Lightyear"
+
+fullName = firstName + " " + lastName  # Concatenation - Combining strings together
+
+print(fullName)
+
+
+
+### Predict and Run Example 3
+ # Add comments to the code below to explain what it does
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
+
+
+### ------------------------ Investigate ------------------------ ###
+
+### Example Code 1
+
+firstName = "Mr"
+lastName = "Anderson"
+
+print(firstName)
+
+# Identify a variable in the code
+ # Answer:
+
+# Identify a string in the code
+ # Answer:
+
+# Line 4 is changed to lastName = "Thorpe".  How does this affect the output?
+ #Answer:
+
+# Line 3 is changed to firstName = "Mrs". How does this affect the output?
+ # Answer:
+
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15    
+total2 = num2 * 2     
+total3 = num1 - num2  
+
+print(total3)    
+
+# What will be output by the program?
+ # Answer:
+
+
+### Example Code 3
+
+name1 = "Ross" 
+name2 = "Monica" 
+name3 = "Joey" 
+name4 = "Rachel" 
+name5 = "Chandler" 
+ 
+print(name1 + " and " + name4) 
+print(name3) 
+ 
+name3 = "Phoebe" 
+
+
+# How many variables are used in the program?
+ # Answer:
+
+# What would be the impact of changing  print(name1, " and ", name4) to print(name1, " and ", name5) ?
+ # Answer:
+
+# What is the purpose of the '+' symbol in  print(name1 + " and " + name4) 
+ # Answer:
+
+# The line  print(name3) is added to the end of the code.  Explain what it will do.
+ # Answer:
+
