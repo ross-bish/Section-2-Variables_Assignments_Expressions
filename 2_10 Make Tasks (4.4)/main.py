10ea60176be86f3b69986b4a674834511f4cf54b
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:13:53 +0000

    Add files via upload

---
+++ 2_10 Make Tasks (4.4)/main.py
@@ -0,0 +1,29 @@
+# -------------------------------------------
+# 4.4 - Make Task
+# Date:
+# --------------------------------------------
+
+
+### Task 1
+# Get input for height & width. Convert to integers and store in variables
+height = input("Enter height: ")
+width = 
+
+# Calculate the area & store the result in the area variable
+# Area of a Rectangle is Length x Width
+area =
+
+# Output the area variable (converted to string)
+
+
+
+### Task 2
+
+
+
+
+### Task 3
+
+
+
+
