e7838f46767816b35f2258a77c3051de644fc44b
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 14:51:53 +0000

    Add files via upload

---
+++ 2.2 Variable Types/main.py
@@ -0,0 +1,21 @@
+# ------------------
+# Variable Types
+# Date: 
+# -------------------
+"""
+x = 5
+y = 3.14
+z = "Pi"
+k = True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(k))
+"""
+x = float(5)
+print(type(x))
+print(x)
+x = 5.1
+print(type(x))
+print(x)
