76a9269321853967e7dbf941ae53b587d718e642
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:03:33 +0000

    Add files via upload

---
+++ 2.4 Make Assignment/main.py
@@ -0,0 +1,34 @@
+# ------------------------
+# Variable Assignment
+# Date: 22/9/22
+# ------------------------
+
+
+
+
+# Assign your name and your favourite food to 2 separate variables with suitable names.
+name = "Ross"
+favouriteFood = "sandwiches"
+
+
+
+
+############# SIMPLE ###################
+# Output the contents of the variables on 2 separate lines
+print(name)
+print(favouriteFood)
+
+
+
+############# Task 1 - MEDIUM ####################
+# Output two sentences (not just the contents of the variables). The first with your name, the second with your favourite food.
+print("My name is", name)
+#print("My name is" + " " + name)
+print(f"My name {name}")
+
+
+
+############# Task 2 - COMPLEX ###################
+# Output both pieces of information as part of the same sentence.
+# Make sure that you have spaces and punctuation in the correct places. 
+print("My name is", name, "and my favourite food is", favouriteFood)
