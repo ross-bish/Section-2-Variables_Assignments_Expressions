640854690304d12dcfe1bda06697db833403f8a1
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:27:39 +0000

    Add files via upload

---
+++ 2_16 Parsons Problem/main.py
@@ -0,0 +1,21 @@
+# ------------------
+# Parson's Problem 
+# Date:
+# ------------------
+
+
+### Challenge 1
+
+
+
+
+
+### Challenge 2
+
+
+
+
+### Questions ###
+# 1. Did you find this type of coding exercise useful? 
+
+# 2. If so why??
